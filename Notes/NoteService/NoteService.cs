4c4604063f5d54318403b3b64fb01cc095dab866
Єлизавета Богачек <bohachek@ukma.edu.ua>
2018-11-24 17:21:23 +0200

    WCF

---
+++ Notes/NoteService/NoteService.cs
@@ -0,0 +1,52 @@
+﻿using Notes.DBAdapter;
+using Notes.DBModels;
+using Notes.ServiceInterface;
+using System;
+using System.Collections.Generic;
+
+namespace Notes.NoteService
+{
+    class NoteService : INoteContract
+    {
+        public bool UserExists(string login)
+        {
+            return EntityWrapper.UserExists(login);
+        }
+
+        public User GetUserByLogin(string login)
+        {
+            return EntityWrapper.GetUserByLogin(login);
+        }
+
+        public User GetUserByGuid(Guid guid)
+        {
+            return EntityWrapper.GetUserByGuid(guid);
+        }
+
+        public void AddUser(User user)
+        {
+            EntityWrapper.AddUser(user);
+        }
+
+        public void AddNote(Note note)
+        {
+            EntityWrapper.AddNote(note);
+        }
+
+        public List<User> GetAllUsers(Guid NoteGuid)
+        {
+            return EntityWrapper.GetAllUsers(NoteGuid);
+        }
+
+        public void DeleteNote(Note selectedNote)
+        {
+            EntityWrapper.DeleteNote(selectedNote);
+        }
+
+
+        public void SaveNote(Note note)
+        {
+            EntityWrapper.SaveNote(note);
+        }
+    }
+}
---
+++ Notes/NoteService/NoteWindowService.cs
@@ -0,0 +1,78 @@
+﻿using Notes.Tools;
+using System;
+using System.ServiceModel;
+using System.ServiceProcess;
+
+namespace Notes.NoteService
+{
+    public class NoteWindowsService : ServiceBase
+    {
+        internal const string CurrentServiceName = "NoteSimulatorService1";
+        internal const string CurrentServiceDisplayName = "Note Simulator Service1";
+        internal const string CurrentServiceSource = "NoteSimulatorServiceSource1";
+        internal const string CurrentServiceLogName = "NoteSimulatorServiceLogName1";
+        internal const string CurrentServiceDescription = "Note Simulator for learning purposes1.";
+        private ServiceHost _serviceHost = null;
+
+        public NoteWindowsService()
+        {
+            ServiceName = CurrentServiceName;
+            try
+            {
+                AppDomain.CurrentDomain.UnhandledException += UnhandledException;
+                Logger.Log("Initialization");
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Initialization", ex);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.Log("OnStart");
+            RequestAdditionalTime(120 * 1000);
+            try
+            {
+                if (_serviceHost != null)
+                    _serviceHost.Close();
+            }
+            catch
+            {
+            }
+            try
+            {
+                _serviceHost = new ServiceHost(typeof(NoteService));
+                _serviceHost.Open();
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("OnStart", ex);
+                throw;
+            }
+            Logger.Log("Service Started");
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Log("OnStop");
+            RequestAdditionalTime(120 * 1000);
+            try
+            {
+                _serviceHost.Close();
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Trying To Stop The Host Listener", ex);
+            }
+            Logger.Log("Service Stopped");
+        }
+
+        private void UnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            var ex = (Exception)args.ExceptionObject;
+
+            Logger.Log("UnhandledException", ex);
+        }
+    }
+}
